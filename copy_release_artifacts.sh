1a9022d9e71bd3ea306768a5c21dc867f81b44af
Casey O'Donnell <caseybrian92@gmail.com>
2018-07-25 16:54:55 +0200

    [release-type final] v1.0.0
    Small change to copy_release_artifacts.sh

--- copy_release_artifacts.sh
+++ copy_release_artifacts.sh
@@ -1,37 +1,39 @@
-# collects release artifacts and copies them to the build Directory
+#!/bin/bash
 
+# collects release artifacts and copies them to the build Directory
+projectFolder="."
 buildName=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}' --non-recursive exec:exec)
 buildVersion=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 # check all is in order
-if [ ! -e target/${buildName}-${buildVersion}-javadoc.jar ]
+if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar ]
 then
-  echo "File target/${buildName}-${buildVersion}-javadoc.jar not found: exiting"
+  echo "File ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar not found: exiting"
   exit 1
 fi
-if [ ! -e target/${buildName}-${buildVersion}-sources.jar ]
+if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar ]
 then
-  echo "File target/${buildName}-${buildVersion}-sources.jar not found: exiting"
+  echo "File ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar not found: exiting"
   exit 1
 fi
-if [ ! -e target/${buildName}-${buildVersion}.jar ]
+if [ ! -e ${projectFolder}/target/${buildName}-${buildVersion}.jar ]
 then
-  echo "File target/${buildName}-${buildVersion}.jar not found: exiting"
+  echo "File ${projectFolder}/target/${buildName}-${buildVersion}.jar not found: exiting"
   exit 1
 fi
 
 # make the builds directory pristine again
-if [ ! -d builds ]
+if [ ! -d ${projectFolder}/builds ]
 then
   echo "Creating builds folder"
-  mkdir builds
+  mkdir ${projectFolder}/builds
 fi
 
 echo "Removing old files"
-rm -rf builds/*
+rm -rf ${projectFolder}/builds/*
 
 # copy artifacts
 echo "Copying artifacts"
-cp target/${buildName}-${buildVersion}-javadoc.jar builds
-cp target/${buildName}-${buildVersion}-sources.jar builds
-cp target/${buildName}-${buildVersion}.jar builds
+cp ${projectFolder}/target/${buildName}-${buildVersion}-javadoc.jar builds
+cp ${projectFolder}/target/${buildName}-${buildVersion}-sources.jar builds
+cp ${projectFolder}/target/${buildName}-${buildVersion}.jar builds
